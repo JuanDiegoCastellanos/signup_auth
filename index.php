b9356c4f70fbd6dfea0da97e7887c5aa24f76443
Juan Diego Castellanos Jerez <juancastellanosj15@gmail.com>
2023-12-15 02:55:03 -0500

    first commit, 'I am testing php if it is working well'

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="berserkRepo.jpg" type="image/x-icon,jpg">
+    <link rel="stylesheet" href="./assets/styles.css" type="text/css">
+    <title>SalesExercise</title>
+</head>
+
+<body>
+    <div class="mundo">
+        <img src="worldMap.png" alt="worldmap" srcset="worldMap.png">
+    </div>
+    <div class="cajaForm">
+        <h2>Login charro</h2>
+        <form action=".login" method="post">
+            <input type="text" name="usernameBox" id="usernameBox" placeholder="Username">
+            <input type="email" name="emailBox" id="emailBox" placeholder="Email">
+            <input type="password" name="passwordBox" id="passwordBox" placeholder="Password">
+            <input type="submit" value="Login">
+        </form>
+    </div>
+</body>
+
+</html>
